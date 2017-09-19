936ad5fd5e67e7c1a96d5a2991383c1c3ff66838
John Bond <github@johnbond.org>
2017-09-19 12:03:27 +0100

    use static IP addresses when using virtualbox. probably easier to move to this everywhere tbh

--- spec/acceptance/05_notify_mixed_os.rb
+++ spec/acceptance/05_notify_mixed_os.rb
@@ -6,9 +6,9 @@ if ENV['BEAKER_TESTMODE'] == 'agent' and ENV['VIRTUALBOX'] == 'yes'
   describe 'notifies with Virtualbox' do
     context 'test notifies' do
       dnsmaster    = find_host_with_role('dnsmaster')
-      dnsmaster_ip = fact_on(dnsmaster, 'ipaddress_eth1')
+      dnsmaster_ip = '10.255.1.3'
       dnsslave     = find_host_with_role('dnsslave')
-      dnsslave_ip  = fact_on(dnsslave, 'ipaddress_eth1')
+      dnsslave_ip  = '10.255.1.4'
       example_zone = <<EOS
 example.com. 3600 IN SOA sns.dns.icann.org. noc.dns.icann.org. 1 7200 3600 1209600 3600
 example.com. 86400 IN NS a.iana-servers.net.
